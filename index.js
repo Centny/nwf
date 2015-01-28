6b77a30aa415ce2336d2220c3b8f36f7988d42f9
Centny <centny@gmail.com>
2015-01-28 22:28:36 +0800

    remove bin ,and adding build.

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ module.exports = {};
 module.exports.version = "0.0.1";
 module.exports.author = "Centny";
 module.exports.buffer = require("./lib/buffer");
+//
 module.exports.netw = require("./lib/netw");
+module.exports.netw.obdh = require("./lib/netw/obdh");
+module.exports.netw.ntdh = require("./lib/netw/ntdh");
+//
+module.exports.im = {};
+module.exports.im.im = require("./lib/im/im");
+module.exports.im.sio = require("./lib/im/sio");
+
 module.exports.desc = function() {
 	console.log("<<------------ buffer ------------>>");
 	console.log(this.buffer.desc);
